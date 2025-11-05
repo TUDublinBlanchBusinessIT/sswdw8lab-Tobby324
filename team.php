41167c365f22d9ef25b64e2dc488dd243b01f0cd
B00165442 Tobby Akinwale <B00165442@mytudublin.ie>
2025-11-05 11:26:46 +0000

    completed part two

--- team.php
+++ team.php
@@ -1,26 +1,36 @@
 <?php
 class Team
 {
-    private $name = "";
-    private $totalGames = 0;
-    private $totalGoals = 0;
-    private $totalPoints = 0;
+   private $name ="";
+   private $totalGoals = 0;
+   private $totalGames = 0;
+   private $totalPoints = 0;
 
     public function __construct($parm1)
     {
         $this->name = $parm1;
     }
 
-    public function finalScore($parm1, $parm2)
-    {
-        $this->totalGames += 1;
-        $this->totalGoals += $parm1;
+   public function finalScore($parm1, $parm2){
+       $this->totalGoals = $this->totalGoals + $parm1;
+       $this->totalGames = $this->totalGames + 1;
+       
+       if($parm1 > $parm2) {
+          $this->totalPoints = $this-> totalPoints +3;
+       }
+            elseif ($parm1 == $parm2) {
+              $this->totalPoints = $this->totalPoints +1;
+            }
+   }
+   public function getTotalPoints() {
+       return $this-> totalPoints;
+   }
+   public function getGoalAverage() {
+       if ($this->totalGames == 0) {
+           return 0;
+       }
+       return $this->totalGoals/ $this->totalGames;
+   }
 
-        if ($parm1 > $parm2) {
-            $this->totalPoints += 3;
-        } else if ($parm1 == $parm2) {
-            $this->totalPoints += 1;
-        }
-    }
 }
 ?>
